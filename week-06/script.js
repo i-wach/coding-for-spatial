02e7cce6548a80f51fb19748811b3743c062aeda
i-wach <iw2216@columbia.edu>
2021-02-22 19:00:29 -0500

    updated HTML, created Javascript function, and added css file

--- week-06/script.js
+++ week-06/script.js
@@ -1,31 +1,34 @@
-// let age = 25
-// let height = 5
+//Class Exercise Part I
 
-// console.log(age)
-// console.log(height)
+let age = 25
+let height = 5
 
-// if (age < 8){
-//     console.log("Check out the Merry-Go-Round. You'll love it!");
-// } else if (age >= 8 && age < 65 && height > 4.5){
-//     console.log("Check out the Roller Coaster. It's awesome!");
-// } else {
-//     console.log("Why not enjoy a float down the Lazy River?");
-// }
+console.log(age)
+console.log(height)
 
-// let noun = "dog"
-// let verb = "bark"
-// let adjective = "silly"
+//Class Exercise Part II
+if (age < 8){
+    console.log("Check out the Merry-Go-Round. You'll love it!");
+} else if (age >= 8 && age < 65 && height > 4.5){
+    console.log("Check out the Roller Coaster. It's awesome!");
+} else {
+    console.log("Why not enjoy a float down the Lazy River?");
+}
 
-// console.log('My ' + noun + ' leaps ' + adjective + ' when I ' + verb + ' a rainbow in the sky');
 
-let nouns = ["dog", "cat", "moose", "bear", "fish"]
-let verbs = ["bark", "cry", "catch", "throw", "punch"]
-let adjectives = ["silly", "rough", "soft", "ambitious", "rambunctious"]
+//Function to randomize array entries and output sentence
+function addText() {
+   //Sentence Generator, setup arrays
+    let nouns = ["dog", "cat", "moose", "bear", "fish"]
+    let verbs = ["bark", "cry", "catch", "throw", "punch"]
+    let adjectives = ["sillily", "roughly", "softly", "ambitiously", "rambunctiously"]
+    // Randomize array output
+    let noun = nouns[Math.floor(Math.random()*nouns.length)];
+    let verb = verbs[Math.floor(Math.random()*verbs.length)];
+    let adjective = adjectives[Math.floor(Math.random()*adjectives.length)];
 
-let noun = nouns[Math.floor(Math.random()*nouns.length)];
-let verb = verbs[Math.floor(Math.random()*verbs.length)];
-let adjective = adjectives[Math.floor(Math.random()*adjectives.length)];
+    let response = 'My ' + noun + ' leaps ' + adjective + ' when I ' + verb + ' a rainbow in the sky';
+    document.getElementById("response").innerHTML = response
+}
 
-// let sentence = `My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky`
 
-console.log('My ' + noun + ' leaps ' + adjective + ' when I ' + verb + ' a rainbow in the sky')
