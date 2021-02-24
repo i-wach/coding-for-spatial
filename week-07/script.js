4eeff2efd284567f30a6f5e3f03b3656becb0c89
i-wach <iw2216@columbia.edu>
2021-02-24 01:49:49 -0500

    added html, js, and css files for week 7 class exercises

---
+++ week-07/script.js
@@ -0,0 +1,20 @@
+console.log('connected')
+
+var first = document.querySelector("#first");
+console.log(first);
+
+// 2. Print just the text of `first`.
+console.log(first.innerText);
+
+// 3. Print the HTML of `first`.
+console.log(first.innerHTML);
+
+// 4. Get the section tag from the page and save it to a variable.
+let section = document.querySelector("section")
+console.log(section)
+
+// 5. Print the text content of the section tag
+console.log(section.textContent)
+
+// 6. Print the HTML content of the section tag
+console.log(section.innerHTML)
