fb882c6ffcbdcba010f078d4285e933b52633fed
i-wach <iw2216@columbia.edu>
2021-02-24 17:05:01 -0500

    updates to homepage: added svg, css, and js for animating background color

---
+++ script.js
@@ -0,0 +1,13 @@
+let lightBulb = document.querySelector('#lightButton');
+
+lightBulb.addEventListener("click", changeLight);
+
+let onOff = "true"
+
+function changeLight() {
+    if (onOff = "true") {
+        body.style.backgroundColor = "darkgray";
+        // document.querySelector("#lightButton").getSVGDocument().setAttribute("filter", "invert(0%) sepia(5%) saturate(0%) hue-rotate(15deg) brightness(88%) contrast(105%)")
+
+    }
+}
