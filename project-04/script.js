9604db424e85ca1d46f89f5cfc64118c275f5be3
i-wach <iw2216@columbia.edu>
2021-03-30 20:41:44 -0400

    Setup html, css, and js for project 4

---
+++ project-04/script.js
@@ -0,0 +1,15 @@
+const sneakers = [
+    {
+        brand: "Nike",
+        name: "Space Hippie 04",
+        price: 130,
+        // how can i have multiple, seperately referenceable entries?
+        material: 'recycled textiles',
+        color: 'grey',
+        occasion: 'running',
+        silohuette: 'runner',
+        source: '?'
+        image: 'assets/img_ns01.jpg';
+        link: 'https://www.nike.com/t/space-hippie-04-mens-shoe-gGWDLk/CZ6398-002?nikemt=true&cp=96413943330_search_%7cPRODUCT_GROUP%7cGOOGLE%7c71700000041489782%7cAll_X_X_X_X-Device_X_Nike-Clearance_X%7c%7cc&gclid=Cj0KCQjwmIuDBhDXARIsAFITC_7aVU7xKEntm8ZG8oHtFXgdkbxl4B6j8JYxGhoSMxdvEIsm2MLOIlcaAn_zEALw_wcB&gclsrc=aw.ds',
+    }
+]
